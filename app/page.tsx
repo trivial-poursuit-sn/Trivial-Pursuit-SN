66021b5d80ca241597e73b08b63a52896fd52562
Erwan <55313521+ErwanExplorer@users.noreply.github.com>
2024-08-08 10:59:36 +0200

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import { Navbar } from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <>
-    <h1>
-      Trivial Pursuit SN
-    </h1>
+   <Navbar/>
    </>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import { ModeToggle } from "./ModeToggle"
+
+export function Navbar() {
+  return (
+    <header className="flex items-center justify-between bg-background px-4 py-3 shadow-sm sm:px-6">
+      <Link href="/" className="text-2xl font-bold" prefetch={false}>
+        Trivial Pursuit SN
+      </Link>
+      <nav className="flex items-center space-x-4 text-sm font-medium">
+        <Link href="/devblog" className="hover:text-primary" prefetch={false}>
+            Devblog
+        </Link>
+        <Link href="/equipe" className="hover:text-primary" prefetch={false}>
+            Équipes
+        </Link>
+        <Link href="/contact" className="hover:text-primary" prefetch={false}>
+          Contact
+        </Link>
+        <ModeToggle/>
+      </nav>
+    </header>
+  )
+}
